303836852ea017919f63742d5045be9c107de246
Leslie Zhai <zhaixiang@loongson.cn>
2018-06-21 11:11:40 +0800

    Treat warning as error.
    https://github.com/Loong-Language/loong-toolchain/issues/3

--- test/test.sh
+++ test/test.sh
@@ -6,9 +6,9 @@ LLVM_HOME=/opt/loong-llvm
 CC=$LLVM_HOME/bin/clang
 
 $CC --version
-$CC empty.loong -S -emit-llvm -o empty.loong.ll
-$CC sillyfunction.loong -S -emit-llvm -o sillyfunction.loong.ll
-$CC inv.loong -S -emit-llvm -o inv.loong.ll
-$CC gates.loong -S -emit-llvm -o gates.loong.ll
-$CC and8.loong -S -emit-llvm -o and8.loong.ll
-$CC mux2.loong -S -emit-llvm -o mux2.loong.ll
+$CC empty.loong -S -emit-llvm -Werror -o empty.loong.ll
+$CC sillyfunction.loong -S -emit-llvm -Werror -o sillyfunction.loong.ll
+$CC inv.loong -S -emit-llvm -Werror -o inv.loong.ll
+$CC gates.loong -S -emit-llvm -Werror -o gates.loong.ll
+$CC and8.loong -S -emit-llvm -Werror -o and8.loong.ll
+$CC mux2.loong -S -emit-llvm -Werror -o mux2.loong.ll
