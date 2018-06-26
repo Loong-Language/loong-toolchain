4a0bc377d7b7555618352e56865b678e0e851e9e
Leslie Zhai <zhaixiang@loongson.cn>
2018-06-26 19:46:37 +0800

    Update testcase for GenVerilog pass.
    https://github.com/Loong-Language/loong-toolchain/issues/11

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-rm -f *.ll *.out
+rm -f *.ll *.v *.out
 
 LLVM_HOME=/opt/loong-llvm
 CC=$LLVM_HOME/bin/clang
+OPT=$LLVM_HOME/bin/opt
+LOONG_LIB=$LLVM_HOME/lib/LoongLLVM.so
 
 $CC --version
+
 $CC empty.loong -S -emit-llvm -Werror -o empty.loong.ll
+
 $CC sillyfunction.loong -S -emit-llvm -Werror -o sillyfunction.loong.ll
+$OPT -S -load $LOONG_LIB -generate-verilog sillyfunction.loong.ll 2> sillyfunction.v > /dev/null
+
 $CC inv.loong -S -emit-llvm -Werror -o inv.loong.ll
 $CC gates.loong -S -emit-llvm -Werror -o gates.loong.ll
 $CC and8.loong -S -emit-llvm -Werror -o and8.loong.ll
